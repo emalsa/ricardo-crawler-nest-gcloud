ade3a8cc11ff5ca6b2c88c221e1868e37cdff171
dni <daniele@nicastro.io>
2022-05-23 10:34:49 +0200

    ricardo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Body, Controller, HttpCode, Logger, Post} from '@nestjs/common';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/article')
+  @HttpCode(200)
+  async getArticleData(@Body() postData): Promise<string> {
+    Logger.debug(postData.url);
+    let htmlData = await this.appService.getArticleData(postData.url);
+    if (htmlData == null) {
+      Logger.error('no html');
+      return;
+    }
+
+    let cheerio = require('cheerio');
+    const $ = cheerio.load(htmlData.content.data, {xmlMode: true});
+    let scriptTags = $('script');
+    let jsonData = null;
+
+    scriptTags.each((index, el,) => {
+      if ($(el).attr('id') == '__NEXT_DATA__' && $(el).text() != null) {
+        jsonData = $(el).text();
+      }
+    });
+
+    if (jsonData == null) {
+      return JSON.parse('{"nodata":"nodata"}');
+    }
+    return JSON.parse(jsonData);
+
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
+import * as fs from "fs";
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    async getArticleData(url: string) {
+        Logger.debug('Start')
+
+        const apiKey = 'ak-2mzd8-5fvkv-8ty2g-63qbv-fxgmd';
+        const phantomJsCloud = require('phantomjscloud');
+        const browser = new phantomJsCloud.BrowserApi(apiKey);
+        return browser.requestSingle({
+                url: url,
+                renderType: 'html',
+                suppressJson: ["meta.trace", "originalRequest", "encoding", "name",],
+                requestSettings: {
+                    waitInterval: 0,
+                    doneWhen: [
+                        {event: 'domReady'},
+                    ],
+                    ignoreImages: true,
+                    disableJavascript: true,
+                    resourceModifier: [
+                        {type: "stylesheet", isBlacklisted: true},
+                        {type: "font", isBlacklisted: true},
+                        {type: "image", isBlacklisted: true},
+                    ],
+                }
+            }, (err, response) => {
+                if (err != null) {
+                    throw err.response;
+                }
+                if (response.content == null) {
+                    Logger.log('Empty response');
+                    return;
+                }
+
+                if (response.statusCode != 200 || response.content.statusCode != 200) {
+                    Logger.log('Non 200 Status code returned');
+                    return;
+                }
+
+                // Logger.log(JSON.parse(response.content.data));
+                // Logger.log(response.statusCode);
+                // Logger.log(response.content.statusCode);
+
+
+                fs.writeFileSync(
+                    'data.html',
+                    response.content.data,
+                );
+
+                return response.content.data;
+
+                // return JSON.parse(response.content);
+                // const buff = new Buffer(response.content.data, 'base64');
+                // fs.writeFileSync('stack-abuse-logo-out.png', buff);
+            }
+        );
+
+    }
 }
